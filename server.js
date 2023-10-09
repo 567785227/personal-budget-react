066e995434f1f4981280ed8b5b3a14be854c7f70
ramsai nimmagadda <ramsainimmagadda@ramsais-Air.lan>
2023-10-09 10:57:43 -0400

    backend files added with necessary changes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const budget = require("./data.json");
+const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = 3001;
 
+app.use(cors());
 app.use ('/', express.static('public'));
 
 
